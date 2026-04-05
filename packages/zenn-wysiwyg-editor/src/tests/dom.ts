6f1c83da8e0670a34806d2f245c6c1b2250338c5
karintou8710 <karintou8710@gmail.com>
2026-04-05 19:21:47 +0900

    refactor: waitSelectionChangeをframeコミット待つだけに

--- packages/zenn-wysiwyg-editor/src/tests/dom.ts
+++ packages/zenn-wysiwyg-editor/src/tests/dom.ts
@@ -7,51 +7,17 @@ export function setSelection(dom: HTMLElement) {
   selection.addRange(range);
 }
 
-// NOTE: 選択範囲がfnの前と後で変わる必要あり
 export async function waitSelectionChange(fn: () => void | Promise<void>) {
-  const maxWait = 2000;
-  const interval = 10;
-  let t = 0;
-
-  const selection = window.getSelection();
-  const beforeRange =
-    selection && selection.rangeCount > 0 ? selection.getRangeAt(0) : undefined;
-
   await fn();
-
-  return new Promise((resolve, reject) => {
-    const timer = setInterval(() => {
-      if (t > maxWait) {
-        clearInterval(timer);
-        reject(new Error('Selection change wait timeout'));
-      }
-
-      const selection = window.getSelection();
-
-      const currentRange =
-        selection && selection.rangeCount > 0
-          ? selection.getRangeAt(0)
-          : undefined;
-
-      // レンジが変更されたかチェック
-      if (!rangesEqual(beforeRange, currentRange)) {
-        clearInterval(timer);
-        resolve(true);
-      }
-      t += interval;
-    }, interval);
-  });
+  await waitForFrameCommit();
 }
 
-// 2つのRangeが同じかどうかを比較する関数
-function rangesEqual(
-  range1: Range | undefined,
-  range2: Range | undefined
-): boolean {
-  return (
-    range1?.startContainer === range2?.startContainer &&
-    range1?.startOffset === range2?.startOffset &&
-    range1?.endContainer === range2?.endContainer &&
-    range1?.endOffset === range2?.endOffset
-  );
+function waitForFrameCommit() {
+  return new Promise<void>((resolve) => {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        resolve();
+      });
+    });
+  });
 }
